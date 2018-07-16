d4ddb71a76119f03370d08c97c5d030c9a5fb6d4
kyle-miho <kyle.miho@liferay.com>
2018-07-16 16:15:48 -0700

    Finished findUnused Macros.pl

--- findUnusedMacros.pl
+++ findUnusedMacros.pl
@@ -8,33 +8,52 @@ print STDOUT ("Starting...");
 #List of macros
 my $macroList = "";
 my $macroTotal = 0;
+my $macroCalls = "";
 my $currentCount = 0;
-#OPEN MACROS DIRECTORY
+
+#START TIME
+my $start_time = time();
+
+#PORTAL-HOME 
+my $portal_home = "L:/public/master-portal";
+
+#GET MACROS USED into MACROLIST, MACROS CALLED into macroCalls
 use File::Find;
-find(\&openFile,"L:/public/master-portal/portal-web/test/functional/com/liferay/portalweb/macros");
+find(\&openFile,"$portal_home/portal-web/test/functional/com/liferay/portalweb/macros");
+find(\&getMacroCallsList,"$portal_home/portal-web/test/functional/com/liferay/portalweb/macros");
+find(\&getMacroCallsList,"$portal_home/portal-web/test/functional/com/liferay/portalweb/tests");
 
 #split macroList and call countMacroUsage
 my @splitMacroList = split(' ', $macroList);
+my @splitMacroCalls = split(' ', $macroCalls);
+
+open(my $fileTemp, '>', 'unusedMacros.txt') or die;
+open(my $fileTemp2, '>', 'usedMacros.txt') or die;
 foreach my $macro (@splitMacroList) {
-    my $temp = countMacroUsages($macro);
-    if (countMacroUsages($macro) == 0)
+    my $temp = countMacroUsages($macro,@splitMacroCalls);
+    if ($temp == 0)
     {
-        open(my $fileTemp, '+>>', 'unusedMacros.txt') or die;
         print $fileTemp $macro . " - Usages: " . $temp . "\n";
-        close($fileTemp);
     } else {
-        open(my $fileTemp2, '+>>', 'usedMacros.txt') or die;
         print $fileTemp2 $macro . " - Usages: " . $temp . "\n";
-        close($fileTemp2);
     }
-    $currentCount++;
     print("\r");
     my $currentCountString = "$currentCount";
     my $macroTotalString = "$macroTotal";
     print STDOUT ("Current Progress: " . $currentCountString . "/" . $macroTotalString);
+    $currentCount++;
     select()->flush();
 }
-print STDOUT ("Done!");
+#CLOSE FILES
+close($fileTemp);
+close($fileTemp2);
+
+#RESULTS POST
+print STDOUT ("\nDone!\n");
+my $end_time = time();
+my $run_time = $end_time - $start_time;
+print STDOUT "Job took $run_time seconds.\n";
+exit;
 
 ###
 #DEF: Open file specified, and add its macros to $macroList
@@ -65,30 +84,13 @@ sub openFile {
 }
 
 ###
-#DEF: Given a Macro, return how many times it is used
+#DEF: Open file specified, and add its macro calls to $macroCalls
 ###
-my $macroCount = 0;
-my $macroNameGlobal = "";
-
-sub countMacroUsages {
-    my ($macroName) = @_;
-
-    $macroCount = 0;
-    #iterate through each file in /macros and count how many times they are used
-    use File::Find;
-    $macroNameGlobal = $macroName;
-    find(\&_countMacroUsage,"L:/public/master-portal/portal-web/test/functional/com/liferay/portalweb/macros");
-    find(\&_countMacroUsage,"L:/public/master-portal/portal-web/test/functional/com/liferay/portalweb/tests");
-    my $temp2 = $macroCount;
-    return $temp2;
-}
-
-sub _countMacroUsage {
+sub getMacroCallsList {
     #if not file, don't try to open it
     if(!(-f $_)){
         return;
     }
-
     #OPEN FILE
     open(my $currentFile, "<", $_)
         or die "Can't open for reading $_";
@@ -97,9 +99,21 @@ sub _countMacroUsage {
     local $/ = undef;
     my $fileContent = <$currentFile>;
 
-    while ($fileContent =~ m/(?<=<execute macro=\")$macroNameGlobal(?=\"(.*?)>)/g) {
-        $macroCount++;
-    } 
+    #remove extension, we don't need anymore
+    $_ =~ s{\.[^.]+$}{};
+
+    #GET MACROS
+    while ($fileContent =~ m/(?<=<execute macro=\")(.*?)(?=\"(.*?)>)/g) {
+        my $macroName = join '#', $_, $&;
+        $macroCalls .= $macroName . "\n";
+    }
     close $currentFile;
-    return;
+}
+
+###
+#DEF: Given a Macro, return how many times it is used
+###
+sub countMacroUsages {
+    my ($macroName, @macroCalls) = @_;
+    return grep(/$macroName/,@macroCalls);
 }
