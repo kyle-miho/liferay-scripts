79cc94d329c5bce52f2f7214bc7bf3fa92fc28ba
kyle.miho <kyle.miho@liferay.com>
2019-07-03 10:57:40 -0700

    linux update

--- git/ghPR.py
+++ git/ghPR.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess, time, sys, os
 
 if len(sys.argv) != 3:
@@ -9,30 +11,43 @@ reviewer=sys.argv[1]
 submitToBranch=sys.argv[2]
 #
 
-portalDirectory = "L:/public/master-portal"
-gitBranch = subprocess.check_output("cd " + portalDirectory + " && git rev-parse --abbrev-ref HEAD",shell=True)
+portalDirectory = "~/Liferay/public/master-portal"
+gitBranch = str(subprocess.check_output("cd " + portalDirectory + " && git rev-parse --abbrev-ref HEAD",shell=True))
 
 wordNum=0
 portalVersion=""
 ticketType=""
 ticketNumber=""
-for i in range(0,len(gitBranch)):
-    if gitBranch[i] == '-':
+
+#get commit message and then get ticketType/number
+recentCommit = str(subprocess.check_output("git log -1 --pretty=%B",shell=True))
+for i in range(0,len(recentCommit)):
+    if recentCommit[i] == '-':
+       wordNum += 1
+    elif recentCommit[i] == ' ':
+        wordNum += 1
+    elif recentCommit[i] == '\'':
         wordNum += 1
     else:
-        if wordNum == 0:
-            portalVersion += gitBranch[i]
         if wordNum == 1:
-            ticketType += gitBranch[i]
+            ticketType += recentCommit[i]
         if wordNum == 2:
-            ticketNumber += gitBranch[i]
+            ticketNumber += recentCommit[i]
 
-if ticketType=='qa':
-    ticketType='LRQA'
-elif ticketType=='lps':
-    ticketType='LPS'
-
+#submit pr and fix description / title
 output = "cd " + portalDirectory + " && gh pr -s " + reviewer + " -b " + submitToBranch + " --description \"https://issues.liferay.com/browse/" + ticketType + "-" + ticketNumber + "\" --title \"" + ticketType + "-" + ticketNumber + " | " + submitToBranch +"\""
 output = output.replace('\n','')
 if subprocess.call(output,shell=True) != 0:
     print("An error has occured, please use Node GH normally to check for the error")
+
+#gh-jira call if reviewer is in reviewer list
+reviewers = {'kyle-miho':'kyle.miho','brianchiu':'brian.chiu','brianchandotcom':'brian.chan','jkapper':'jurgen.kappler'}
+
+#call gh jira if applicable
+if reviewer not in reviewers:
+    print("No Jira account associated with github account in reviewers dictionary, not updating ticket")
+    exit()
+
+jiraName = reviewers[reviewer]
+subprocess.call("gh jira " + ticketType + "-" + ticketNumber + " --assignee " + jiraName + " --transition \"Submit for Review\"",shell=True)
+subprocess.call("gh jira " + ticketType + "-" + ticketNumber + " --browser")
